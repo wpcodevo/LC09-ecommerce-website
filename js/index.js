79f8404d2930e29ec7d22a0f8a901b10f0f131e7
Edem <ziddah.edem@gmail.com>
2021-06-26 11:54:54 +0000

    adding all files

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const hamburer = document.querySelector(".hamburger");
+const navList = document.querySelector(".nav-list");
+
+if (hamburer) {
+  hamburer.addEventListener("click", () => {
+    navList.classList.toggle("open");
+  });
+}
+
+// Popup
+const popup = document.querySelector(".popup");
+const closePopup = document.querySelector(".popup-close");
+
+if (popup) {
+  closePopup.addEventListener("click", () => {
+    popup.classList.add("hide-popup");
+  });
+
+  window.addEventListener("load", () => {
+    setTimeout(() => {
+      popup.classList.remove("hide-popup");
+    }, 1000);
+  });
+}
---
+++ js/slider.js
@@ -0,0 +1,14 @@
+// Hero Carousel
+const slider1 = document.querySelector("#glide_1");
+if (slider1) {
+  new Glide(slider1, {
+    type: "carousel",
+    startAt: 0,
+    autoplay: 3000,
+    gap: 0,
+    hoverpause: true,
+    perView: 1,
+    animationDuration: 800,
+    animationTimingFunc: "linear",
+  }).mount();
+}
